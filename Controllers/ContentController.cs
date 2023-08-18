629370fec3fa195e0b826e31a0231df03fda3fa6
Needlide <max.litvinyuck553@gmail.com>
2023-08-18 23:04:12 +0300

    Deleted unnecessary items from the Visual Studio template. Configured CORS, versioning, middleware for development (Swagger, developer exception page). Renamed controller.

---
+++ Controllers/ContentController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace StellarStreamAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContentController : ControllerBase
+    {
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,29 +1,45 @@
+using Microsoft.AspNetCore.Mvc.Versioning;
+
 string AllowedOriginsPolicyName = "AllowedAnyOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: AllowedOriginsPolicyName, policy => { policy.AllowAnyOrigin(); });
+    options.AddPolicy(name: AllowedOriginsPolicyName, policy => { policy.AllowAnyOrigin().AllowCredentials().AllowAnyHeader(); });
 });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApiVersioning(options =>
+{
+    options.RegisterMiddleware = true;
+    options.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = new HeaderApiVersionReader("api-version");
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors(AllowedOriginsPolicyName);
 
 app.UseAuthorization();
 
